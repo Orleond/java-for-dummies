0d6bbc0a0bb6cd8978f5472115e4c49f45ba3b52
Orleond <mr.orleond@gmail.com>
2024-12-22 16:33:06 +0300

    Chapter 1

---
+++ src/main/java/com/orleond/ChaptersChapter1/PayBarry.java
@@ -0,0 +1,11 @@
+package com.orleond.ChaptersChapter1;
+
+public class PayBarry {
+    public static void main(String[] args) {
+        double checkAmount = 1257.63;
+        System.out.print("Оплатить по распоряжению ");
+        System.out.print("Барри Берда ");
+        System.out.print("$");
+        System.out.print(checkAmount);
+    }
+}
