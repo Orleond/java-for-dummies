b0f71f150519961b177827026669f2e4b270d621
Orleond <mr.orleond@gmail.com>
2024-12-22 17:21:33 +0300

    Adding Chapter 3

--- src/main/java/com/orleond/Chapters/Chapter3/Displayer.java
+++ src/main/java/com/orleond/Chapters/Chapter3/Displayer.java
@@ -1,26 +1,7 @@
 package com.orleond.Chapters.Chapter3;
 
-/*
- * Листинг 3.6 из книги "Java для чайников"
- *
- * Copyright 2017 Wiley Publishing Inc.
- * Все права защищены
- */
-
-/**
- *
- * @author  Barry Burd
- * @version 1.0 1/24/17
- * @see     java.lang.System
- */
-
 public class Displayer {
-    /**
-     * Метод main, с которого начинается выполнение кода
-     *
-     * @param args (см. главу 11)
-     */
     public static void main(String[] args) {
-        System.out.println("Вам понравится Java!"); // Мне? Нам?
+        System.out.println("Вам понравится Java!");
     }
 }
---
+++ src/main/java/com/orleond/Chapters/Chapter3/Displayer2.java
@@ -0,0 +1,26 @@
+package com.orleond.Chapters.Chapter3;
+
+/*
+ * Листинг 3.6 из книги "Java для чайников"
+ *
+ * Copyright 2017 Wiley Publishing Inc.
+ * Все права защищены
+ */
+
+/**
+ *
+ * @author  Barry Burd
+ * @version 1.0 1/24/17
+ * @see     java.lang.System
+ */
+
+public class Displayer2 {
+    /**
+     * Метод main, с которого начинается выполнение кода
+     *
+     * @param args (см. главу 11)
+     */
+    public static void main(String[] args) {
+        System.out.println("Вам понравится Java!"); // Мне? Нам?
+    }
+}
